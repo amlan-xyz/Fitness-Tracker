119d60d92a0e8fe5150ae63b1ed6748a11249850
theweird0ne <heyamlan19@gmail.com>
2023-11-10 12:33:16 +0530

    fix: in-progress error fixed in goal entry

--- client/src/pages/Goals/Goals.jsx
+++ client/src/pages/Goals/Goals.jsx
@@ -1,69 +1,72 @@
-import {useEffect} from "react";
-import { useDispatch,useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 //components
 import { AddGoal } from "../../components/Forms/GoalEntryForm";
 
 //actions
-import {fetchGoals,deleteGoals} from '../../actions/goals.actions'
+import { deleteGoals, fetchGoals } from "../../actions/goals.actions";
 
 //icons
-import { AiOutlineDelete } from 'react-icons/ai';
+import { AiOutlineDelete } from "react-icons/ai";
 import { Loader } from "../../components/Loader/Loader";
 
-export const Goals=()=>{
-    const dispatch=useDispatch();
-    const goals=useSelector(state=>state.goals)
-    const loading=useSelector(state=>state.loading);
+export const Goals = () => {
+  const dispatch = useDispatch();
+  const goals = useSelector((state) => state.goals);
+  const loading = useSelector((state) => state.loading);
 
-    const handleDelete=(goalId)=>{
-        dispatch(deleteGoals(goalId));
-    }
+  const handleDelete = (goalId) => {
+    dispatch(deleteGoals(goalId));
+  };
 
-    const convertDate=(date)=>{
-        const timeStamp = new Date(date);
-	    const newTime = timeStamp.toDateString();
-        return newTime;
-    }
+  const convertDate = (date) => {
+    const timeStamp = new Date(date);
+    const newTime = timeStamp.toDateString();
+    return newTime;
+  };
 
-    useEffect(()=>{
-        dispatch(fetchGoals());
-    },[dispatch]);
+  useEffect(() => {
+    dispatch(fetchGoals());
+  }, [dispatch]);
 
-    return(
-        <section className="section">
-                        <header className="section__header">
-                <p className="section__header-p">Goals</p>
-                
-            </header>
-        
-            <AddGoal/>
-        
-            <div className="section__container">
-            {
-                loading===true?<Loader/>:<ul className="list">
-                {
-                    goals && goals.map(goal=>(
-                        <li className="list-item" key={goal._id}>
-                            <button onClick={()=>handleDelete(goal._id)}><AiOutlineDelete/></button>
-                            <h3>
-                               {goal.goal_name}
-                            </h3>
-                            <p>{goal.goal_description}</p>
-                            <p>{
-                                convertDate(goal.target_date)    
-                            }</p>
-                            <p>{goal.target_calories} calories</p>
-                            <p className={goal.goal_status==="in-progress"?"highlight-red":"highlight-green"}>{goal.goal_status}</p>
-                        </li>
-                    ))
-                }
-            </ul>
-            }
-            </div>
-            
-        </section>
-    )
+  return (
+    <section className="section">
+      <header className="section__header">
+        <p className="section__header-p">Goals</p>
+      </header>
 
-}
+      <AddGoal />
 
+      <div className="section__container">
+        {loading === true ? (
+          <Loader />
+        ) : (
+          <ul className="list">
+            {goals &&
+              goals.map((goal) => (
+                <li className="list-item" key={goal._id}>
+                  <button onClick={() => handleDelete(goal._id)}>
+                    <AiOutlineDelete />
+                  </button>
+                  <h3>{goal.goal_name}</h3>
+                  <p>{goal.goal_description}</p>
+                  <p>{convertDate(goal.target_date)}</p>
+                  <p>{goal.target_calories} calories</p>
+                  <p
+                    className={
+                      goal.goal_status === "in-progress"
+                        ? "highlight-red"
+                        : "highlight-green"
+                    }
+                  >
+                    {goal.goal_status}
+                  </p>
+                </li>
+              ))}
+          </ul>
+        )}
+      </div>
+    </section>
+  );
+};
